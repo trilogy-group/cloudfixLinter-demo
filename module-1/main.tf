06668bd4a6066e3870c41b0ac5ffb565ccbe39f5
Arleif Braganza <32668779+arleif-dfactory@users.noreply.github.com>
2022-08-02 23:01:11 +1000

    Update main.tf

--- module-1/main.tf
+++ module-1/main.tf
@@ -2,11 +2,15 @@ resource "aws_instance" "showcase-1" {
 
   ami           = "ami-09d56f8956ab235b3"
   instance_type = var.web_instance_type
-  
+  tags = {    
+    created_for = "cloudfix-linter demo"
+  }
 }
 
 resource "aws_s3_bucket" "b" {
   bucket_prefix = "my-tf-bucket-cloudfixlinter"
-
+  tags = {    
+    created_for = "cloudfix-linter demo"
+  }
   
 }
