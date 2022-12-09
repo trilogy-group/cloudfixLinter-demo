ec96ae3b5eb0ebc923d24d4379fa7c9bffccd512
prasheel-ti <prasheel.tiwari@trilogy.com>
2022-12-10 00:41:40 +0530

    Creating New Demo

--- auth-module/main.tf
+++ auth-module/main.tf
@@ -2,8 +2,9 @@ resource "aws_instance" "app-server" {
 
   ami           = "ami-09d56f8956ab235b3"
   instance_type = var.web_instance_type
-  subnet_id     = "subnet-0103f016fd921604d"
+  subnet_id     = "subnet-0ad82a9a46e5aaf68"
   tags = {
+    owner       = "prasheel.tiwari@trilogy.com"
     created_for = "cloudfix-linter demo"
   }
 }
@@ -15,3 +16,14 @@ resource "aws_s3_bucket" "b" {
   }
 
 }
+
+# resource "aws_cloudtrail" "cloudtrail" {
+#   name                          = "tf-trail-test"
+#   s3_bucket_name                = aws_s3_bucket.b.id
+#   s3_key_prefix                 = "prefix"
+#   include_global_service_events = false
+#   tags = {
+#     created_for = "cloudfix-linter demo"
+#     yor_trace = "385b58f7-8bbc-47e9-af0f-998d00a5850c"
+#   }
+# }
--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "app-server" {
   instance_type = var.ec2-instance
   ami           = "ami-09d56f8956ab235b3"
-  subnet_id     = "subnet-0103f016fd921604d"
+  subnet_id     = "subnet-0ad82a9a46e5aaf68"
   tags = {
     created_for = "cloudfix-linter demo"
   }
@@ -27,6 +27,88 @@ resource "aws_ebs_volume" "config-vol" {
   #type              = "gp2"
   tags = {
     created_for = "cloudfix-linter demo"
+  }
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = "vpc-02badd8abb988e06e"
+  service_name = "com.amazonaws.us-east-1.s3"
+  tags = {
+    created_for = "cloudfix-linter demo"
+  }
+}
+
+resource "aws_nat_gateway" "example" {
+  connectivity_type = "private"
+  subnet_id         = "subnet-0ad82a9a46e5aaf68"
+  tags = {
+    created_for = "cloudfix-linter demo"
+  }
+}
+
+resource "aws_neptune_cluster" "default" {
+  cluster_identifier                   = "neptune-cluster-development"
+  engine                               = "neptune"
+  engine_version                       = "1.2.0.1"
+  neptune_cluster_parameter_group_name = "default.neptune1.2"
+  skip_final_snapshot                  = true
+  apply_immediately                    = true
+  tags = {
+    created_for = "cloudfix-linter demo"
+  }
+}
+
+resource "aws_dynamodb_table" "cloudfix-linter" {
+  name         = "cloudfixlinter"
+  hash_key     = "TestTableHashKey"
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "TestTableHashKey"
+    type = "S"
+  }
+  tags = {
+    created_for = "cloudfix-linter demo"
+  }
+}
+
+module "ec2_instance" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+
+  count = 1
+
+  name = "single-instance"
+
+  ami           = "ami-09d56f8956ab235b3"
+  instance_type = "t2.micro"
+  monitoring    = true
+  subnet_id     = "subnet-0ad82a9a46e5aaf68"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+module "s3_bucket_remote" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-remote-s3-bucket"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+  tags = {
+    owner     = "prasheel.tiwari@trilogy.com"
+  }
+}
+
+module "remote_module_s3" {
+  source = "s3::https://s3.amazonaws.com/remote-module-cflint-prasheel/remote-module.zip"
+  tags = {
+    owner     = "prasheel.tiwari@trilogy.com"
   }
 }
 
