2ecf8433cdc7d3d743830c80036aab00c228846f
Ankushpandey-ti <ankush.pandey@trilogy.com>
2023-01-17 15:12:29 +0530

    ami and vpc id referred from variables

--- variables.tf
+++ variables.tf
@@ -6,8 +6,18 @@ variable "id" {
   type = string
 }
 
+variable "app_server_subnet_id" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
 variable "subnet_id" {
   type = string
 }
 
-
+variable "ami" {
+  type = string
+}
