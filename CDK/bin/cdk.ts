5cbc83abefb0bce4ff82aa3e24f98ceff5ad2cb5
Ankush Pandey <ankush.pandey@trilogy.com>
2023-03-21 13:32:33 +0000

    tags added

--- CDK/bin/cdk.ts
+++ CDK/bin/cdk.ts
@@ -2,9 +2,10 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { CdkStack } from '../lib/cdk-stack';
+import { Tags } from 'aws-cdk-lib';
 
 const app = new cdk.App();
-new CdkStack(app, 'CdkStack', {
+const stack = new CdkStack(app, 'CdkStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -19,3 +20,6 @@ new CdkStack(app, 'CdkStack', {
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
+
+Tags.of(stack).add("Owner", "ankush.pandey@trilogy.com");
+Tags.of(stack).add("Project","cloudfix-linter-cdk");
