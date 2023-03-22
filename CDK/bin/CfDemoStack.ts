de4dd5be64bf318a5c976d61ca416d777a4a9f00
Ankushpandey-ti <ankush.pandey@trilogy.com>
2023-03-22 14:05:54 +0530

    cdk stack remaned to cfdemostack

--- CDK/bin/CfDemoStack.ts
+++ CDK/bin/CfDemoStack.ts
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkStack } from '../lib/cdk-stack';
+import { CfDemoStack } from '../lib/CfDemoStack';
 import { Tags } from 'aws-cdk-lib';
 
 const app = new cdk.App();
-const stack = new CdkStack(app, 'CdkStack', {
+const stack = new CfDemoStack(app, 'CfDemoStack', {
+  stackName: `CfDemoStack`,
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
