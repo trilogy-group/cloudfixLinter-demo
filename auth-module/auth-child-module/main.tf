a661f54a629c5da11f66bdd04713a6c0a85a2d53
yashg-ti <133036599+yashg-ti@users.noreply.github.com>
2023-07-12 12:41:49 +0530

    auth-child-modifi

--- auth-module/auth-child-module/main.tf
+++ auth-module/auth-child-module/main.tf
@@ -6,7 +6,7 @@ variable "ebs_device_type" {
 resource "aws_ebs_volume" "data-vol" {
   availability_zone = "us-east-1a"
   size              = 1
-  type              = var.ebs_device_type         // change this to xx to save yy dollars for a resource with a module path: root->child->grandchild...   
+  type              = var.ebs_device_type         
   tags = {
     created_for = "cloudfix-linter demo"
     Owner = "ankush.pandey@trilogy.com"
